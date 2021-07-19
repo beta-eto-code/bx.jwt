55f0749bc139d515da4426b67fb01acd9cfa2d68
Alexander Nesterov <alex19pov31@gmail.com>
2021-07-19 17:21:32 +0300

    fix(installer) update installer

--- install/index.php
+++ install/index.php
@@ -87,7 +87,7 @@ class bx_jwt extends CModule
         }
 
         require_once $modulePath;
-        $moduleInstaller = new bx_router();
+        $moduleInstaller = new bx_model();
         $resultInstall = (bool)$moduleInstaller->DoInstall();
         if (!$resultInstall) {
             $this->setError('Ошибка установки модуля bx.model');
