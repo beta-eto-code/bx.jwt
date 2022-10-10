e5cea6a90bd839a260c30369d18b373e2219a747
Alexander Nesterov <alex19pov31@gmail.com>
2022-10-10 16:18:54 +0300

    feat(sign) update sig strategy - encapsulate private key

--- lib/strategy/basestrategy.php
+++ lib/strategy/basestrategy.php
@@ -7,8 +7,18 @@ use Bx\JWT\Interfaces\TokenStrategyInterface;
 
 abstract class BaseStrategy implements TokenStrategyInterface
 {
+    /**
+     * @var string
+     */
+    private $key;
+
+    public function __construct(?string $key = null)
+    {
+        $this->key = $key ?? (string)Option::get('bx.jwt', 'JWT_SECRET');
+    }
+
     protected function getKey(): string
     {
-        return (string)Option::get('bx.jwt', 'JWT_SECRET');
+        return $this->key;
     }
 }
